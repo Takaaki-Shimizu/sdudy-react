7aefec83eaadae52e2ffa477193ca71f2b6a54ee
Takaaki-Shimizu <tattaka_feelmysoul@yahoo.co.jp>
2022-12-31 17:02:07 +0900

    ✨ useCallback, useEffectの第2引数についての挙動確認

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,14 +3,15 @@ import classes from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(1);
-  const handleClick = (e) => {
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-  };
+  const handleClick = useCallback(() => {
+    if (count < 10) {
+      setCount((count) => count + 1);
+    }
+  }, [count]);
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
@@ -19,8 +20,6 @@ export default function Home() {
       document.body.style.backgroundColor = "";
     };
   }, []);
-
-  console.log(count);
 
   return (
     <div className={classes.container}>
