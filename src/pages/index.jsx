9fe43946c205b057f37d9b21c629712ae4347215
Takaaki-Shimizu <tattaka_feelmysoul@yahoo.co.jp>
2022-12-31 16:06:52 +0900

    ⚗️ 不要な記述でいつか消しますが、試しにクリックイベントを書いてみました

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,14 +3,25 @@ import classes from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
+import { useCallback } from "react";
+import Link from "next/link";
 
 export default function Home() {
+  const foo = 1;
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
   return (
     <div className={classes.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
+      <Link href="/about" onClick={handleClick}>
+        ボタン
+      </Link>
       <Main page="index" />
       <Footer />
     </div>
